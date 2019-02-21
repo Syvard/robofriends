cb268899190580e83125c347d3406f84fccd0225
Chris <chris.northwood@smashpotatoes.com>
2019-02-21 14:22:59 -0800

    updated to loop through a robots array and display each item as card

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Chris',
+    username: 'WutMan101',
+    email: 'Chris@chris.co'
+  },
+  {
+    id: 2,
+    name: 'David',
+    username: 'AppleDave',
+    email: 'Dave@thebigapple.co'
+  },
+  {
+    id: 3,
+    name: 'Nathan',
+    username: 'NoobTube70',
+    email: 'Noob@tube.com'
+  },
+  {
+    id: 4,
+    name: 'Emilee',
+    username: 'Erri',
+    email: 'erri404@google.co'
+  },
+  {
+    id: 5,
+    name: 'Andrea',
+    username: 'Roflstompr',
+    email: 'ANStomper@Stomp.com'
+  },
+  {
+    id: 6,
+    name: 'Mike',
+    username: 'AirGuitarGod',
+    email: 'MichaelStahp@youcant.ca'
+  },
+  {
+    id: 7,
+    name: 'Nadia',
+    username: 'Chungus',
+    email: 'SuchaBig@chungus.com'
+  },
+  {
+    id: 8,
+    name: 'Noah',
+    username: 'Books4Me',
+    email: 'Iliketoread@booksbooksbooks.ca'
+  },
+  {
+    id: 9,
+    name: 'KB',
+    username: 'SunBear',
+    email: 'Dog@cutedog.io'
+  },
+  {
+    id: 10,
+    name: 'Will',
+    username: 'BigHairCut',
+    email: 'Will@office.io'
+  }
+]
